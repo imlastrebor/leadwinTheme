e11e527d4f2a031b69f6d54834902be1d91c3c08
Robert Salmi <44033010+imlastrebor@users.noreply.github.com>
2021-11-17 19:17:37 +0200

    Setup

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function load_stylesheets()
+{
+    wp_register_style('styles', get_template_directory_uri() . '/styles/styles.css', array(), 1, 'all');
+    wp_enqueue_style('styles');
+
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Alumni+Sans:wght@500&family=Josefin+Sans:wght@400;600&display=swap', false );
+}
+add_action('wp_enqueue_scripts', 'load_stylesheets');
+
+?>
